aa6bdaec36fbf425cadc3c02e13c129ba4dad673
Kofi Boateng <kdboat2@gmail.com>
2022-10-28 20:57:54 -0500

    Added concurrent projects to repo.

---
+++ ActorParallelism/src/main/java/edu/coursera/concurrent/SieveActor.java
@@ -0,0 +1,106 @@
+package edu.coursera.concurrent;
+
+import edu.rice.pcdp.Actor;
+import static edu.rice.pcdp.PCDP.finish;
+
+/**
+ * An actor-based implementation of the Sieve of Eratosthenes.
+ *
+ * TODO Fill in the empty SieveActorActor actor class below and use it from
+ * countPrimes to determine the number of primes <= limit.
+ */
+public final class SieveActor extends Sieve {
+    /**
+     * {@inheritDoc}
+     *
+     * TODO Use the SieveActorActor class to calculate the number of primes <=
+     * limit in parallel. You might consider how you can model the Sieve of
+     * Eratosthenes as a pipeline of actors, each corresponding to a single
+     * prime number.
+     */
+    @Override
+    public int countPrimes(final int limit) {
+        final SieveActorActor sieveActor = new SieveActorActor(2);
+        finish(()->{
+            for(int i =3; i < limit; i+=2){
+                sieveActor.send(i);
+            }
+            sieveActor.send(0);
+        });
+
+        int numPrimes = 0;
+        SieveActorActor looper = sieveActor;
+        while(looper != null){
+            numPrimes+= looper.getNumLocalPrimes();
+            looper = looper.getNextActor();
+        }
+
+        return numPrimes;
+    }
+
+    /**
+     * An actor class that helps implement the Sieve of Eratosthenes in
+     * parallel.
+     */
+    public static final class SieveActorActor extends Actor {
+        /**
+         * Process a single message sent to this actor.
+         *
+         * TODO complete this method.
+         *
+         * @param msg Received message
+         */
+
+        private static final int MAX_LOCAL_PRIMES = 500;
+        private final int localPrimes[];
+        private int numLocalPrimes;
+        private SieveActorActor nextActor;
+
+        SieveActorActor(final int localPrime) {
+            this.localPrimes = new int[MAX_LOCAL_PRIMES];
+            this.localPrimes[0] = localPrime;
+            this.numLocalPrimes = 1;
+            this.nextActor = null;
+        }
+
+        public SieveActorActor getNextActor() {return  nextActor;}
+        public int getNumLocalPrimes() {return numLocalPrimes;}
+
+        @Override
+        public void process(final Object msg) {
+            final int candidate =(Integer)  msg;
+            if(candidate <= 0){
+                if(nextActor != null){
+                    nextActor.send(msg);
+                }
+            }else {
+                final boolean isLocallyPrime = isLocallyPrime(candidate);
+                if(isLocallyPrime){
+                    if(numLocalPrimes < MAX_LOCAL_PRIMES){
+                        localPrimes[numLocalPrimes] = candidate;
+                        numLocalPrimes +=1;
+                    }else if(nextActor == null){
+                        nextActor = new SieveActorActor(candidate);
+                    }else{
+                        nextActor.send(msg);
+                    }
+                }
+            }
+        }
+
+        private boolean isLocallyPrime(int candidate) {
+            return checkPrimeKernel(candidate, numLocalPrimes);
+        }
+
+        private boolean checkPrimeKernel(int candidate, int end) {
+            for(int i = 0; i < end; i++){
+                if(candidate % localPrimes[i] == 0){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+    }
+}
